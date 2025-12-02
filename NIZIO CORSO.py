f5d476b9c08c1e412eb38421226eee7ed802bf53
Artiux <artiux26@gmail.com>
2025-12-02 22:53:28 +0100

    Tipi di dati

---
+++ NIZIO CORSO.py
@@ -0,0 +1,19 @@
+x =7
+y= 3
+somma = x + y
+print (somma)
+
+prezzo = 19.99
+quantita = 3
+Totale = prezzo * quantita
+print (Totale)
+
+variabile = 10
+print ( variabile > 5 )
+print ( variabile < 9)
+
+a = 5
+b = 2.5
+
+somma = a + b
+print(somma)
