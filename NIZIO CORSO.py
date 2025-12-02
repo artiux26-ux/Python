8b3b4a2304f754b1c2bddedd5394f9d6716502a1
Artiux <artiux26@gmail.com>
2025-12-02 23:57:16 +0100

    Esercizi e sorgenti - Casting

--- NIZIO CORSO.py
+++ NIZIO CORSO.py
@@ -16,4 +16,16 @@ a = 5
 b = 2.5
 
 somma = a + b
+
 print(somma)
+
+# Esercizio e sorgenti casting
+numero = input("Inserisci un numero intero: ")
+
+# Converti in float
+numero_float = float(numero)
+print("Numero in formato float:", numero_float)
+
+# Converti in stringa e stampa con un messaggio
+numero_stringa = str(numero)
+print("Hai inserito il numero: " + numero_stringa)
