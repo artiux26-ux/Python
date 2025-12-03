1fe48f7b8078bca49d683e9f48039278da5cd8fc
Artiux <artiux26@gmail.com>
2025-12-03 12:24:02 +0100

    esercizi

---
+++ Esercizi variabili e tipi di dati.py
@@ -0,0 +1,70 @@
+# Esercizio 1 stampa variabili
+nome = "Aldo"
+eta = 30
+citta = "Berlino"
+print ( nome, eta, citta )
+
+# Esercizio 2 sostituzione variabile
+x =7
+print ( x )
+x = 10
+print ( x )
+
+# Esercizio 3 somma variabili
+
+x =7
+y= 34
+somma = x + y
+print (somma)
+
+# Esercizio 4 somma numeri
+a = 20
+b = 15
+a,b = b,a
+print (a,b)
+
+# Esercizio 5 area rettangolo
+
+base = 5
+altezza = 10
+area = (base * altezza) 
+print ("Area rettangolo:", area)
+
+# Esercizio 6 somma float e int
+c= 10
+d = 3.5
+print ( c + d )
+
+# Esercizio 7 Calcolo media
+e, f, g = 12, 30, 45
+media = (e + f + g) / 3
+print ( "La media è:", media )
+
+# Esercizio 8 concatenazione stringhe
+s1 = "Buongiorno"
+s2 = " a tutti!"
+print ( s1 + s2 )
+
+# Esercizio 9 Moltiplicazione stringa
+print ( "Allora" * 3 )
+
+# Esercizio 10 Booleani
+
+h = 15
+i = 30
+l = 34
+
+print ( h < i )  
+print ( i > l )
+print ( h < l ) 
+
+# casting esercizi
+
+l = 10
+m = float(l)
+print ( m)
+
+# conversione numero a stringa
+n = 4550
+o = str(n)
+print ( "Il numero è :",  o )
